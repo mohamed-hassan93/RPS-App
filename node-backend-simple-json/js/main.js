8af9fd34c84d7d59affc2ef07344b7ca860ace3f
Ahmed Alimohamed <ahmedali@Ahmeds-MacBook-Air.local>
2022-06-06 18:13:53 -0700

    When the user wants to pick a choice multiple times, the hidden toggles on and off instead of keeping the icon on the screen. Issue resolved.

--- node-backend-simple-json/js/main.js
+++ node-backend-simple-json/js/main.js
@@ -9,29 +9,48 @@ Array.from(options).forEach((element) =>
 );
 
 let user_input;
+let user_rock;
+let user_paper;
+let user_scissor;
 //this function grabs the user input and displays the animation. The function then calls the function run() which gathers the informaiton from our API
 function userchoice(click) {
   if (click.target.classList.contains("rock")) {
     user_input = "rock";
-    document.querySelector('#player-rock').classList.toggle('hidden')
-    document.querySelector('#player-paper').classList.add('hidden')
-    document.querySelector('#player-scissors').classList.add('hidden')
-
+    if (user_rock == true) {
+      document.querySelector("#player-rock").classList.remove("hidden");
+    } else {
+      document.querySelector("#player-rock").classList.toggle("hidden");
+    }
+    document.querySelector("#player-paper").classList.add("hidden");
+    document.querySelector("#player-scissors").classList.add("hidden");
+    user_rock = true;
   } else if (click.target.classList.contains("paper")) {
     user_input = "paper";
-    document.querySelector('#player-paper').classList.toggle('hidden')
-    document.querySelector('#player-rock').classList.add('hidden')
-    document.querySelector('#player-scissors').classList.add('hidden')
-
+    if (user_paper == true) {
+      document.querySelector("#player-paper").classList.remove("hidden");
+    } else {
+      document.querySelector("#player-paper").classList.toggle("hidden");
+    }
+    document.querySelector("#player-rock").classList.add("hidden");
+    document.querySelector("#player-scissors").classList.add("hidden");
+    user_paper = true;
   } else if (click.target.classList.contains("scissors")) {
     user_input = "scissors";
-    document.querySelector('#player-scissors').classList.toggle('hidden')
-    document.querySelector('#player-paper').classList.add('hidden')
-    document.querySelector('#player-rock').classList.add('hidden')
-
+    if (user_scissor == true) {
+      document.querySelector("#player-scissors").classList.remove("hidden");
+    } else {
+      document.querySelector("#player-scissors").classList.toggle("hidden");
+    }
+    document.querySelector("#player-paper").classList.add("hidden");
+    document.querySelector("#player-rock").classList.add("hidden");
+    user_scissor = true;
   }
   run();
 }
+
+let computer_rock;
+let computer_paper;
+let computer_scissor;
 async function run() {
   //Here we are fetching the API from our server.js file
   const res = await fetch(`/api?student=${user_input}`);
@@ -39,18 +58,33 @@ async function run() {
   //We grab the value from  json
   const bot_choice = data.result;
 
-  if (bot_choice === 'rock'){
-    document.querySelector('#bot-rock').classList.toggle('hidden')
-    document.querySelector('#bot-paper').classList.add('hidden')
-    document.querySelector('#bot-scissors').classList.add('hidden')
-  } else if (bot_choice === 'paper'){
-    document.querySelector('#bot-paper').classList.toggle('hidden')
-    document.querySelector('#bot-rock').classList.add('hidden')
-    document.querySelector('#bot-scissors').classList.add('hidden')
-  } else{
-    document.querySelector('#bot-scissors').classList.toggle('hidden')
-    document.querySelector('#bot-paper').classList.add('hidden')
-    document.querySelector('#bot-rock').classList.add('hidden')
+  if (bot_choice === "rock") {
+    if (computer_rock == true) {
+      document.querySelector("#bot-rock").classList.remove("hidden");
+    } else {
+      document.querySelector("#bot-rock").classList.toggle("hidden");
+    }
+    document.querySelector("#bot-paper").classList.add("hidden");
+    document.querySelector("#bot-scissors").classList.add("hidden");
+    computer_rock = true;
+  } else if (bot_choice === "paper") {
+    if (computer_rock == true) {
+      document.querySelector("#bot-paper").classList.remove("hidden");
+    } else {
+      document.querySelector("#bot-paper").classList.toggle("hidden");
+    }
+    document.querySelector("#bot-rock").classList.add("hidden");
+    document.querySelector("#bot-scissors").classList.add("hidden");
+    computer_paper = true;
+  } else {
+    if (computer_scissor == true) {
+      document.querySelector("#bot-scissors").classList.remove("hidden");
+    } else {
+      document.querySelector("#bot-scissors").classList.toggle("hidden");
+    }
+    document.querySelector("#bot-paper").classList.add("hidden");
+    document.querySelector("#bot-rock").classList.add("hidden");
+    computer_scissor = true;
   }
 
   //Here we decide if the outcome of the match was a draw, win for the user, or a loss
