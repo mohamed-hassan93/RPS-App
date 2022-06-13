11c5fa1026b40c2d10addd9afc1b22b0185614f8
Mohamed <42161293+mohamed-hassan93@users.noreply.github.com>
2022-06-12 21:27:33 -0400

    file organization

---
+++ node-backend-simple-json/serverFiles/server.js
@@ -0,0 +1,55 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const querystring = require("querystring");
+const figlet = require("figlet");
+
+const server = http.createServer((req, res) => {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == "/") {
+    fs.readFile("index.html", function (err, data) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else if (page == "/api") {
+    if ("student" in params) {
+      if (params["student"] == "rock" || "paper" || "scissors") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        //Here we generate a random bot choice for our game
+        let options = ["rock", "paper", "scissors"];
+        const botChoice = options[Math.floor(Math.random() * options.length)];
+        const objToJson = {
+          result: botChoice,
+        };
+       
+        res.end(JSON.stringify(objToJson));
+      } 
+    } 
+  } else if (page == "/css/style.css") {
+    fs.readFile("css/style.css", function (err, data) {
+      res.write(data);
+      res.end();
+    });
+  } else if (page == "/js/main.js") {
+    fs.readFile("js/main.js", function (err, data) {
+      res.writeHead(200, { "Content-Type": "text/javascript" });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    figlet("404!!", function (err, data) {
+      if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(8000);
